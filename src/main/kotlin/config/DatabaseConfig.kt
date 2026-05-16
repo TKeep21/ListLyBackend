f2e78687d664a552fe2f5578ee7f4df05d5aec60
TKeep21 <mahach2006@mail.ru>
2026-05-16 21:05:45 +0300

    feat(DbIndexes): added

--- src/main/kotlin/config/DatabaseConfig.kt
+++ src/main/kotlin/config/DatabaseConfig.kt
@@ -5,9 +5,12 @@ import com.example.UserMedia.model.UserMediaItem
 import com.example.media.model.MediaItem
 import com.example.user.User
 import org.litote.kmongo.KMongo
+import com.mongodb.MongoCommandException
 import org.litote.kmongo.getCollection
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoDatabase
+import com.mongodb.client.model.IndexOptions
+import com.mongodb.client.model.Indexes
 import io.ktor.server.application.Application
 import io.ktor.server.application.ApplicationStopping
 import io.ktor.server.application.log
@@ -32,6 +35,61 @@ object DatabaseConfig {
     fun userFolders() = database.getCollection<UserFolder>()
     fun globalMediaItems() = database.getCollection<MediaItem>()
 
+    fun ensureIndexes() {
+        ensureIndex(
+            collectionName = "users",
+            indexName = "ux_users_login",
+            indexDefinition = Indexes.ascending("login"),
+            options = IndexOptions().unique(true).name("ux_users_login")
+        )
+
+        ensureIndex(
+            collectionName = "userMediaItems",
+            indexName = "ux_userMedia_userId_mediaId",
+            indexDefinition = Indexes.compoundIndex(
+                Indexes.ascending("userId"),
+                Indexes.ascending("mediaId")
+            ),
+            options = IndexOptions().unique(true).name("ux_userMedia_userId_mediaId")
+        )
+
+        ensureIndex(
+            collectionName = "globalMediaItems",
+            indexName = "ix_media_title",
+            indexDefinition = Indexes.ascending("title"),
+            options = IndexOptions().name("ix_media_title")
+        )
+
+        ensureIndex(
+            collectionName = "globalMediaItems",
+            indexName = "ix_media_mediaType",
+            indexDefinition = Indexes.ascending("mediaType"),
+            options = IndexOptions().name("ix_media_mediaType")
+        )
+
+        ensureIndex(
+            collectionName = "userFolders",
+            indexName = "ix_userFolders_userId",
+            indexDefinition = Indexes.ascending("userId"),
+            options = IndexOptions().name("ix_userFolders_userId")
+        )
+    }
+
+    private fun ensureIndex(
+        collectionName: String,
+        indexName: String,
+        indexDefinition: org.bson.conversions.Bson,
+        options: IndexOptions
+    ) {
+        try {
+            database.getCollection<org.bson.Document>(collectionName)
+                .createIndex(indexDefinition, options)
+        } catch (ex: MongoCommandException) {
+            // Keep app startup resilient if existing data violates a newly-added unique index.
+            println(">>> Failed to create index '$indexName' on '$collectionName': ${ex.errorMessage}")
+        }
+    }
+
     fun close() = client.close()
 }
 
@@ -39,10 +97,11 @@ object DatabaseConfig {
 
 fun Application.configureDatabase() {
     DatabaseConfig.init()
+    DatabaseConfig.ensureIndexes()
 
     environment.monitor.subscribe(ApplicationStopping) {
         DatabaseConfig.close()
     }
 
-    log.info("Database connected successfully with KMongo")
+    log.info("Database connected successfully with KMongo, indexes ensured")
 }
